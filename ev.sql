f172d2c7451be79cde76d545f8fa62045937cdf2
Ruthvik08 <84057266+Ruthvik08@users.noreply.github.com>
2025-04-11 12:18:30 +0530

    Update ev.sql

--- ev.sql
+++ ev.sql
@@ -35,27 +35,31 @@ select * from state;
 -- 1. List the top 3 and bottom 3 makers for the fiscal years 2023 and 2024 in
 -- terms of the number of 2-wheelers sold.
 with c1 as (select * from maker where cate='2-Wheelers' and (date like '2023%' or date like '2024%'))
-(select maker,sum(ev_sold) from c1
+(select maker,sum(ev_sold) as total_ev_sold from c1
 group by maker
 order by sum(ev_sold) desc
 limit 3)
-union
-(select maker,sum(ev_sold) from c1
+union all
+select 'bottom 3' as maker,'' total_ev_sold
+union all
+(select maker,sum(ev_sold) as total_ev_sold from c1
 group by maker
-order by sum(ev_sold) group by state
-
+order by sum(ev_sold)
 limit 3);
 
 -- 2. Identify the top 5 states with the highest penetration rate in 2-wheeler
 -- and 4-wheeler EV sales in FY 2024.
-(select state,((sum(evcate)/sum(totat_sales))*100) as pent from state 
+(select state,((sum(evcate)/sum(totat_sales))*100) as penetration from state 
 where date like '2024%' and cate='2-Wheelers'
 group by state
 order by ((sum(evcate)/sum(totat_sales))*100) desc
 limit 5)
 union 
-(select state,((sum(evcate)/sum(totat_sales))*100) as pent from state 
+select '4 wheeler' as state , '-------------------' as penetration
+union
+(select state,((sum(evcate)/sum(totat_sales))*100) as penetration from state 
 where date like '2024%' and cate='4-Wheelers'
+group by state
 order by ((sum(evcate)/sum(totat_sales))*100) desc
 limit 5);
 
@@ -72,11 +76,49 @@ with c4count as (select maker,sum(ev_sold) as sev from maker
 				where cate='4-Wheelers' and (date like '2022%' or date like '2023%' or date like '2024%')
 				group by maker 
 				order by sum(ev_sold) desc limit 5)
-
 select maker.maker,date.year1,date.quater,sum(maker.ev_sold) from maker
 right join c4count on maker.maker=c4count.maker
 right join date on date.date1=maker.date
-group by maker.maker,date.year1,date.quater;
+where maker.maker is not null
+group by maker.maker,date.year1,date.quater
+order by maker.maker desc;
+
+-- 5 How do the EV sales and penetration rates in Delhi compare to
+-- Karnataka for 2024?
+
+select state ,sum(evcate) as total_evsales,((sum(evcate)/sum(totat_sales))*100) as  penetration from state
+where state in ('Delhi','Karnataka')
+group by state;
+
+-- 6 List down the compounded annual growth rate (CAGR) in 4-wheeler
+-- units for the top 5 makers from 2022 to 2024.
+
+with c1 as (SELECT maker,SUM(ev_sold) AS total_ev_sold,
+RANK() OVER (ORDER BY SUM(ev_sold) DESC) AS sales_rank FROM maker where cate='4-Wheelers' GROUP BY maker  limit 5),
+c2 as (select maker.maker,year(maker.date) as year,sum(maker.ev_sold) as sum_ev from c1
+inner join maker on c1.maker=maker.maker
+where maker.date in (2022,2024)
+group by maker.maker,year(maker.date)
+order by maker.maker,year(maker.date) desc)
+select maker , power(sum_ev/(lead(sum_ev) over (partition by maker order by maker,year desc)),1/2)-1 as cagr,sum_ev,year from c2 ;
+
+-- 7 List down the top 10 states that had the highest compounded annual
+-- growth rate (CAGR) from 2022 to 2024 in total vehicles sold.
+
+with c1 as (select distinct year(date) as year,state,sum(totat_sales) over (partition by year(date),state order by state,year(date) desc  ) as sales1 from state
+where year(date) in (2022,2024) )
+select c1.state,power((c2.sales2024/c1.sales1),1/3)-1 as cagr from c1
+inner join (select sales1 as sales2024,state from c1 where year=2024) c2 on c1.state=c2.state
+where c1.year=2022
+order by power((c2.sales2024/c1.sales1),1/2)-1 desc
+limit 10;
+
+-- 8 What are the peak and low season months for EV sales based on the data from 2022 to 2024?
+
+select month(date) as month, monthname(date) as month ,sum(evcate) as ev_sales from state
+where  year(date) between 2022 and 2024 
+group by month(date), monthname(date)
+order by sum(evcate);
 
 
 
